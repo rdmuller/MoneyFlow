b24d57f559798bae3372cc352a9f260120d72d72
Ronald D. Müller <46840910+rdmuller@users.noreply.github.com>
2026-04-02 17:45:03 -0300

    20260402

---
+++ src/core/SharedKernel/Abstractions/Error.cs
@@ -0,0 +1,6 @@
+﻿namespace SharedKernel.Abstractions;
+
+public record Error(string Code, string Description)
+{
+    public static Error None => new(string.Empty, string.Empty);
+}
---
+++ src/core/SharedKernel/Abstractions/Result.cs
@@ -0,0 +1,22 @@
+﻿namespace SharedKernel.Abstractions;
+
+public class Result
+{
+    protected internal Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None)
+            throw new InvalidOperationException("A successful result cannot have an error.");
+
+        if (!isSuccess && error == Error.None)
+            throw new InvalidOperationException("A failure result must have an error.");
+
+        Error = error;
+        IsSuccess = isSuccess;
+    }
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; }
+}
